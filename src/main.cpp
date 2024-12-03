326c38a6303f4c0803af29a2373739ef5f7a5af9
Cameron Planck <cameronplanck@gmail.com>
2024-12-03 15:27:02 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "Arduino.h"
+
+int count = 0;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  count ++;
+  Serial.println(count);
+}
